a0d17990a4e78aa5577e247b772811b8b9f54033
Joel Fuentes <jfuentes@drew.edu>
2024-11-06 13:06:25 -0500

    empty page commit

--- app.py
+++ app.py
@@ -96,7 +96,20 @@ def article1():
 def logout():
     logout_user()  # Log out the user
     flash('You have been logged out', 'success')
-    return redirect(url_for('login'))
+    return redirect(url_for('login')) 
+
+
+
+
+
+
+
+@app.route('/emptypage')  
+@login_required  
+def emptypage():  
+     return render_template('emptypage.html')  
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
