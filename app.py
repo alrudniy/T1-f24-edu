adba27e473ca03ed61ef2a7d6fb25267626c32ae
Francesca Okleasky <fokleasky@drew.edu>
2024-11-08 12:03:16 -0500

    forgot password

--- app.py
+++ app.py
@@ -73,6 +73,50 @@ def create_account():
         return redirect(url_for('login'))
     return render_template('create_account.html')
 
+from flask import Flask, render_template, request, redirect, url_for, flash
+from itsdangerous import URLSafeTimedSerializer  # For generating tokens
+
+# Secret key for generating tokens
+s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        username = request.form['username']
+        user = db_session.query(User).filter_by(username=username).first()
+        
+        if user:
+            # Generate token for the user
+            token = s.dumps(user.username, salt='password-reset-salt')
+            reset_url = url_for('reset_password', token=token, _external=True)
+            
+            # Send this URL to the user via email (pseudo-code below)
+            # send_email(user.email, reset_url)
+            flash('A password reset link has been sent to your email.', 'info')
+        else:
+            flash('User not found.', 'error')
+    
+    return render_template('forgot_password.html')
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        username = s.loads(token, salt='password-reset-salt', max_age=3600)  # Token is valid for 1 hour
+    except:
+        flash('The reset link is invalid or has expired.', 'error')
+        return redirect(url_for('forgot_password'))
+    
+    if request.method == 'POST':
+        user = db_session.query(User).filter_by(username=username).first()
+        if user:
+            new_password = generate_password_hash(request.form['password'], method='scrypt')
+            user.password = new_password
+            db_session.commit()
+            flash('Your password has been updated!', 'success')
+            return redirect(url_for('login'))
+    
+    return render_template('reset_password.html')
+
 @app.route('/pick_a_path')
 @login_required  # Ensure this route is only accessible to logged-in users
 def pick_a_path():
